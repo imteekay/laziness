3df088119c8164396529bd57a959442c4417a036
TK <leandrotk100@gmail.com>
2020-06-07 19:03:03 -0300

    Testing estree parse

--- packages/type-based-test/src/index.ts
+++ packages/type-based-test/src/index.ts
@@ -1,6 +1,12 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
+import { promises as fs } from 'fs';
+import { resolve } from 'path';
+import { parse } from '@typescript-eslint/typescript-estree';
+
+const start = async () => {
+  const sourcePath = resolve(__dirname, '../examples/sum.ts');
+  const source = await fs.readFile(sourcePath, 'utf8');
+  const program = parse(source);
+  console.log(program);
 };
+
+start();
