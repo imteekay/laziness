6501de8c311b4a8e9eb2d11214ebdb4c92d65dd5
TK <leandrotk100@gmail.com>
2020-08-01 18:13:50 -0300

    Test

--- packages/dts-gen/src/index.ts
+++ packages/dts-gen/src/index.ts
@@ -1,6 +1,31 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
+import * as parser from '@babel/parser';
+// import traverse from '@babel/traverse';
+
+const code = `
+SampleComponent.propTypes = {
+  arrayProp: PropTypes.array,
+  boolProp: PropTypes.bool.isRequired,
+  numberProp: PropTypes.number,
+  objectProp: PropTypes.object,
+  stringProp: PropTypes.string,
+  anyProp: PropTypes.any,
+  elementProp: PropTypes.element,
+  nodeProp: PropTypes.node,
+  arrayOfString: PropTypes.arrayOf(PropTypes.string),
+  onChange: PropTypes.func.isRequired,
+  oneOfString: PropTypes.oneOf(['foo', 'bar']),
+	oneOfStringOrNumber: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+	shapeProp: PropTypes.shape({
+		foo: PropTypes.number,
+		bar: PropTypes.string
+  }).isRequired,
+  /**
+	 * @param {object} value - The param value.
+	 */
 };
+`;
+
+export function test() {
+  const ast = parser.parse(code);
+  console.log(ast.program.body[0]);
+}
---
+++ packages/dts-gen/src/tests/index.test.ts
@@ -0,0 +1,7 @@
+import { test } from '../index';
+
+describe('test', () => {
+  it('', () => {
+    test();
+  });
+});
