e78c7e704d24f77c4708c26572b611f5270e7aff
TK <leandrotk100@gmail.com>
2020-08-02 01:28:45 -0300

    Traversing ast

--- packages/dts-gen/src/index.ts
+++ packages/dts-gen/src/index.ts
@@ -1,5 +1,6 @@
+import { Node, Identifier, MemberExpression } from '@babel/types';
 import * as parser from '@babel/parser';
-// import traverse from '@babel/traverse';
+import traverse from '@babel/traverse';
 
 const code = `
 SampleComponent.propTypes = {
@@ -25,7 +26,57 @@ SampleComponent.propTypes = {
 };
 `;
 
+function isIdentifier(node: Node): node is Identifier {
+  return node.type === 'Identifier';
+}
+
+function isMemberExpression(node: Node): node is MemberExpression {
+  return node.type === 'MemberExpression';
+}
+
 export function test() {
   const ast = parser.parse(code);
-  console.log(ast.program.body[0].expression);
+
+  const result: string[] = [];
+
+  traverse(ast, {
+    enter(path) {
+      if (
+        path.node.type === 'ExpressionStatement' &&
+        path.node.expression.type === 'AssignmentExpression' &&
+        path.node.expression.right.type === 'ObjectExpression'
+      ) {
+        path.node.expression.right.properties.forEach(property => {
+          if (property.type === 'ObjectProperty') {
+            let string = '';
+
+            if (isIdentifier(property.key)) {
+              // console.log(property.key.name);
+              string += property.key.name;
+            }
+
+            if (
+              isMemberExpression(property.value) &&
+              isIdentifier(property.value.object)
+            ) {
+              // console.log(property.value.object.name);
+              string += property.value.object.name;
+            }
+
+            if (
+              isMemberExpression(property.value) &&
+              isIdentifier(property.value.property)
+            ) {
+              // console.log(property.value.property.name);
+              string += property.value.property.name;
+            }
+
+            result.push(string);
+          }
+        });
+
+        console.log(result);
+      }
+    },
+  });
 }
