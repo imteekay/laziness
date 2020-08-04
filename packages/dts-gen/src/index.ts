9f16d95f3d416caf27a8be00a3ce264ddf2435f4
TK <leandrotk100@gmail.com>
2020-08-04 09:43:30 -0300

    Mapping prop types to a TS interface object

--- packages/dts-gen/src/index.ts
+++ packages/dts-gen/src/index.ts
@@ -1,18 +1,18 @@
 import { promises as fs } from 'fs';
 import { resolve } from 'path';
-import { parse } from 'react-docgen';
+import { parse, Prop } from 'react-docgen';
 
 async function readFile() {
   const componentFilePath = resolve(__dirname, 'Sample.js');
   return await fs.readFile(componentFilePath, 'utf8');
 }
 
-// type User = {
+// interface User = {
 //   name: string;
 //   age: number;
 // };
 
-// type SampleProps = {
+// interface SampleProps = {
 //   any?: any;
 //   isVisible: boolean;
 //   index?: number;
@@ -28,12 +28,64 @@ async function readFile() {
 //   user: User,
 // };
 
+type PropAttrs = {
+  name: string;
+  required: boolean;
+};
+
+type Result = {
+  [key: string]: PropAttrs;
+};
+
+type PropMapperType = {
+  [key: string]: string;
+};
+
+const PropMapper: PropMapperType = {
+  any: 'any',
+  bool: 'boolean',
+  number: 'number',
+  string: 'string',
+  object: 'object',
+  element: 'React.ReactElement<any>',
+  node: 'React.ReactNode',
+  array: 'any[]',
+  arrayOf: '[]',
+  func: '(event: object) => void',
+  enum: '',
+  union: '',
+  shape: '',
+};
+
+function isPropType(prop: Prop, propType: string) {
+  return prop.type.name === propType;
+}
+
+function buildProp(prop: Prop) {
+  if (isPropType(prop, 'arrayOf')) {
+    const propValue = prop.type.value as Prop;
+    const propName = prop.type.name;
+    return {
+      name: PropMapper[propValue.name] + PropMapper[propName],
+      required: prop.required,
+    };
+  }
+
+  return {
+    name: PropMapper[prop.type.name] || prop.type.name,
+    required: prop.required,
+  };
+}
+
 export async function generate() {
   const component = await readFile();
   const componentAST = parse(component);
+  const result: Result = {};
 
   for (const [propName, prop] of Object.entries(componentAST.props)) {
-    console.log(propName, '-->', prop.type.name);
     console.log(prop);
+    result[propName] = buildProp(prop);
   }
+
+  console.log(result);
 }
